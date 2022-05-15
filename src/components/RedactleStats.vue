56f6244aefd2aa32a80d55d787f6a71290f3af81
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2022-05-15 02:53:46 +0200

    Salut

--- src/components/RedactleStats.vue
+++ src/components/RedactleStats.vue
@@ -16,21 +16,21 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="container container-lg" id="winText" style="display: block;">
-    <h3>Congratulations, you solved Redactle #38!</h3>
+  <div class="container container-lg" style="display: block;">
+    <h3>Félicitation vous avez résolu le Redactus #2 (BETA)!</h3>
     <ul>
-      <li>The answer was: abraham</li>
-      <li>You solved it in 1 guesses</li>
+      <li>La réponse était: Néerlandais</li>
+      <!-- <li>You solved it in 1 guesses</li>
       <li>Your accuracy was 100.00%</li>
-      <li>You have solved 1 consecutive Redactles</li>
+      <li>You have solved 1 consecutive Redactles</li> -->
     </ul>
-    <h3>Global Stats</h3>
+    <!-- <h3>Global Stats</h3> -->
     <!-- <ul>
       <li>Globally, 5909 players have solved today's Redactle so far</li>
       <li>Global Median: 73.00 Guesses; 60.58% Accuracy</li>
       <li>Global Average: 92.04 Guesses; 61.37% Accuracy</li>
     </ul> -->
-    <p><a @click="shareResults">Share your results</a></p>
+    <!-- <p><a @click="shareResults">Share your results</a></p> -->
   </div>
 </template>
 
