753b7f33f72ce403846374f04ff5bb89d782ae8b
Drake62610 <drake@DESKTOP-LJ8P0SK.localdomain>
2022-05-21 05:26:58 +0200

    Push env variables

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import RedactleInterface from "@/components/RedactleInterface.vue"
 import { createRouter, createWebHistory } from "vue-router";
-console.log(import.meta.env.BASE_URL)
+console.log(import.meta.env)
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: "/",
